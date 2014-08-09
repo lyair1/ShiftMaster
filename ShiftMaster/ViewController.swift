7ae37c9143d02d89609fd7ecbc7892e2a47e3bc6
yair levi <lyair1@gmail.com>
2014-08-09 16:24:11 +0300

    main page

--- ShiftMaster/ViewController.swift
+++ ShiftMaster/ViewController.swift
@@ -8,18 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: SMUIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
